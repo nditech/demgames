6e2571d383d544233a5983e37f273f2a7625ae7f
Viet Nguyen <vnguyen@ndi.org>
2018-07-27 08:57:51 -0400

    feat: add cup, new build

--- client/src/pages/Done/Done.js
+++ client/src/pages/Done/Done.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Wrap} from '../../components/Grid'
 import {GameWrap} from '../../components/GameItem'
+import cup from '../../img/cup.png'
 // import photo from '../../img/done.png'
 // import API from '../../utils/API'
 
@@ -16,6 +17,7 @@ class Done extends Component {
                         <div className="col m-0 p-0">
                             <h3 className="m-0">¡FELICIDADES!</h3>
                             <p><small>¡Has terminado el juego!</small></p>
+                            <div className="done-cup"><img src={cup} alt="cup"/></div>
                         </div>
                     </div>
                 </GameWrap>
