9ef50a10c21d3a2bf4de088c583860cba81b3b95
Viet Nguyen <vietjul@gmail.com>
2018-08-29 15:27:10 -0400

    feat: module card a tag

--- client/src/components/Card/ModuleCard.js
+++ client/src/components/Card/ModuleCard.js
@@ -2,14 +2,9 @@ import React from 'react'
 import './Card.css'
 
 export const ModuleCard = ({href, module, colour, title}) => (
-    <div className={`card module-card`}>
-        <a href={href}>
-            <div className="text-center"><b>{title}</b></div>
-            
-        </a>
+    <a href={href} className={`card module-card`}>
+        <div className="text-center module-card-title"><b>{title}</b></div>
         <div className={`module-glass glass-${colour}`}></div>
         <div className={`module-bg ${module}`}></div>
-        
-        {/* <a href={href} className={`card-text text-center bg-${textBg}`}></a> */}
-    </div>
+    </a>
 )
