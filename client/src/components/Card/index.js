75189e3b8ad16a57beb292e88a8a8adc9e60ff8d
jnarang <jatin.narang@hashedin.com>
2019-02-15 12:31:10 +0530

    Refractor: Refractor code

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 export const Card = (props) => (
 	<button
-		className={`card-button card-button-${props.answerClick} card-button-${props.selectedCard} color-change-${props.selectedCard} ${props.color}`}
+		className={`card-button card-button-${props.moduleColor} card-button-${props.answerClick} card-button-${props.selectedCard} color-change-${props.moduleColor}-${props.selectedCard} ${props.color}`}
 		type="button"
 		onClick={props.handleClick}
 	>
