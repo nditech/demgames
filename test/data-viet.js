0d55fe30a1c35b9608b8ffb79e8f161b76d41107
Viet Nguyen <vietjul@gmail.com>
2019-04-02 13:21:55 -0400

    chore: update data-viet.js

--- test/data-viet.js
+++ test/data-viet.js
@@ -21,6 +21,10 @@ const database = [
         "module-2"
     ]
 },
+/** Items can have the same id (partition key), but different type (sort key) 
+ * So we can query for different types of the same user,
+ * or different users that share the same score by using a certain type.
+*/
 {
     "id": "user-1", /** Partition Key - same user id but different composite Primary Key because of Sort Key */
     "type": "user-report", /** Sort Key */
@@ -28,10 +32,38 @@ const database = [
     "scores": []
 },
 {
+    "id": "user-1", // Partition key.
+    "type": "user", // Sort key.
+    "name": "John",
+    "number": "+12345678901"
+},
+{
+    "id": "user-1", // Partition key.
+    "type": "userprogress", // Sort key.
+    "game": [],
+    "modules": []
+},
+{
     "id": "report-1", /** Can even have a new Global Secondary Index to query */
     "type": "report-details", /** New Sort Key */
     "game": "game-3", /** New Partition Key */
     "scores": []
+},
+{ /** Can also implement report entry contains reports for each game or event */
+    "id": "report-1",
+    "type": "report",
+    "event": "event-1",
+    "game": "game-1",
+    "users": [
+        "user-1",
+        "user-3",
+        "user-19"
+    ],
+    "scores": { /** The average score or progress of all users */
+        "module-1": "52.45%",
+        "module-2": "14.5%"
+    }
+    // Can also add more meta data: % of genders, age group, etc.
 },
 {
     "id": "question-1",
@@ -50,46 +82,5 @@ const database = [
     "name": "event 1 name",
     "location": "some address",
     "game": "game-1" /**ties an event with one game */
-},
-{
-    "id": "user-1", // Partition key.
-    "type": "user", // Sort key.
-    "name": "John",
-    "number": "+12345678901",
-    "scores": {
-        "game-1": {
-            "module-1": "80%",
-            "module-2": "70%",
-            "module-3": "60%"
-        },
-        "game-2": {
-            // game scores
-        }
-    },
-    "progress": { /** object if users play multiple games at once. if 1 game, value */
-        "game-1": {
-            "module-1": "question-2",
-            "module-3": "question-1"
-        },
-        "game-2": {
-            "module-1": "question-1"
-        }
-    }
-},
-{ /** Can also implement report entry contains reports for each game or event */
-    "id": "report-1",
-    "type": "report",
-    "event": "event-1",
-    "game": "game-1",
-    "users": [
-        "user-1",
-        "user-3",
-        "user-19"
-    ],
-    "scores": { /** The average score or progress of all users */
-        "module-1": "52.45%",
-        "module-2": "14.5%"
-    }
-    // Can also add more meta data: % of genders, age group, etc.
 }
 ];
