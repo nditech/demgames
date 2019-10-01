5d9e488e1a577f9e3a5651eed6026342b2879d0e
pankaj <pankaj.singh@hashedin.com>
2019-10-01 20:17:27 +0530

    Modified resultPage and added cohort functionality

--- client/src/pages/ResultPage/ResultPage.js
+++ client/src/pages/ResultPage/ResultPage.js
@@ -73,7 +73,7 @@ class ResultPage extends Component {
 			expression
 		} = this.props.location.state;
 
-		// const totalLevels = this.props.gameData.gameData[moduleId - 1].levels.length;
+		const totalLevels = this.props.gameData.gameData[moduleId - 1].levels.length;
 		const backToLevelUrl = `/module/${moduleId}/levels`;
 		const retryLevelUrl = `/module/${moduleScenario ? 'scenario/' : ''}${moduleId}/level/${level}/questions`;
 		const nextLevelUrl = `/module/${moduleScenario ? 'scenario/' : ''}${moduleId}/level/${level + 1}/questions`;
